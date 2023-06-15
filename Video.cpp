b503257b39ec49203e51aba5b3ebdbd82fc873a7
A01799928 <A01799928@tec.mx>
2023-06-14 22:10:35 -0600

    Se implementan los métodos para clase Video

---
+++ Video.cpp
@@ -0,0 +1,43 @@
+#include "Video.h"
+
+Video::Video(string id,string nombre,double duracion,string genero,double calificacion,string fechaEstreno)
+{
+    this -> id = id;
+    this -> nombre = nombre;
+    this -> duracion = duracion;
+    this -> genero = genero;
+    this -> calificacion = calificacion;
+    this -> fechaEstreno = fechaEstreno;
+
+}
+
+
+string Video::getId()
+{
+    return id;
+}
+
+string Video::getNombre()
+{
+    return nombre;
+}
+
+double Video::getDuracion()
+{
+    return duracion;
+}
+
+string Video::getGenero()
+{
+    return genero;
+}
+
+double Video::getCalificacion()
+{
+    return calificacion;
+}
+
+string Video::getFechaEstreno()
+{
+    return fechaEstreno;
+}
