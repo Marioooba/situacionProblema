31aa51afa40f1d5dbb7036a91bba8338de2a08c0
A01799928 <A01799928@tec.mx>
2023-06-16 20:53:03 -0600

    Constructor de la clase Pelicula

---
+++ Pelicula.cpp
@@ -0,0 +1,6 @@
+#include "Pelicula.h"
+
+Pelicula::Pelicula(string id, string nombre, double duracion, string genero, double calificacion, string fechaEstreno) : 
+Video(id, nombre, duracion, genero, calificacion, fechaEstreno)
+{
+}
