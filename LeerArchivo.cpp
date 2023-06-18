3135a40a1ac07d77af9935b94ba9c2e85490cf9e
A01799928 <A01799928@tec.mx>
2023-06-17 21:57:11 -0600

    Funciones para leer archivo y separar sus lineas
    Se modifico la funcion hecha en clase y se implemento otra que no arrojara tanta basura

--- LeerArchivo.cpp
+++ LeerArchivo.cpp
@@ -3,17 +3,73 @@ MARIO ALBERTO PÉREZ BARRERA
 
 LEER EL ARCHIVO DE TEXTO EN C++
 */
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 using namespace std;
 
-vector<string> separar(string linea);
 
+int leerArchivo(string line) {
+  int contadorElementos = 0;      // Contador de elementos
+  int ultimoDigito;   // Índice del último dígito encontrado
+
+  for (int i = 0; i < line.length(); i++) {
+    // Si se encuentra una coma y el carácter anterior no es una coma
+    if (line[i] == ',' && line[i - 1] != ',') {
+      contadorElementos++;                  // Incrementar el contador
+      ultimoDigito = i;         // Actualizar el índice del último dígito encontrado
+    }
+  }
+
+  // Verificar si hay un elemento adicional después de la última coma
+  if (line[ultimoDigito + 1] != ',') {
+    contadorElementos++;        // Incrementar el contador
+  }
+
+  return contadorElementos;     // Devolver la cantidad de elementos encontrados
+}
+
+vector<string> separar(const string &input, char delimiter) {
+  vector<string> tokens;     // Vector para almacenar las subcadenas resultantes
+  istringstream iss(input);  // Objeto istringstream para procesar la cadena de texto
+  string token;              // Variable para almacenar cada subcadena
+
+  // Mientras se pueda obtener una subcadena del istringstream usando el delimitador
+  while (getline(iss, token, delimiter)) {
+    tokens.push_back(token);  // Agregar la subcadena al vector de subcadenas
+  }
+
+  return tokens;             // Devolver el vector con las subcadenas resultantes
+}
+
+string quitarEspacios(const string& str) {
+  size_t start = str.find_first_not_of(' ');           // Posición del primer carácter no espacial
+  size_t end = str.find_last_not_of(' ');              // Posición del último carácter no espacial
+
+  if (start == string::npos || end == string::npos) {
+    // La cadena está compuesta completamente por espacios en blanco
+    return "";
+  }
+
+  return str.substr(start, end - start + 1);           // Extraer la subcadena sin los espacios en blanco al inicio y al final
+}
+
+
+//FUNCION IMPLEMENTADA DURANTE LA CLASE, FUE CAMBIADA DEBIDO A QUE MOSTRABA MUCHA BASURA, SE BUSCO LA OPTIMIZACION
+
+/*
 int main(int argc, char const *argv[])
 {
-    ifstream entrada;
+    string archivo;
+    cout << "Ingresa el nombre del archivo en formato CSV, Ejemplo: (''MiArchivo.csv''): " << endl;
+    cin >> archivo;
+    ifstream entrada(archivo);
+    if (!entrada){
+        cout << "No se pudo abrir el archivo" << endl;
+        return 1;
+    }
 
     // Excepciones
     // entrada.flags(ios_base::failbit);
@@ -21,7 +77,7 @@ int main(int argc, char const *argv[])
 
     try
     {
-        entrada.open("DatosPeliculas.csv");
+        entrada.open(archivo);
     }
     catch (ifstream::failure &e)
     {
@@ -35,7 +91,7 @@ int main(int argc, char const *argv[])
             cout << "Error, no puedo leer el archivo..." << endl;
             return -1;
         }
-    */
+    *
     string linea;
     int numeroLinea = 1;
 
@@ -62,7 +118,7 @@ int main(int argc, char const *argv[])
         {
             break;
         }
-        */
+        *
     }
     try
     {
@@ -109,3 +165,5 @@ vector<string> separar(string linea)
     return tokens;
     // cout << "Tokens: " << numeroTokens << endl << endl;
 }
+
+*/
