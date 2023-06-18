44f5600364aba0c5eee9507f3de486ee44978bc2
A01799928 <A01799928@tec.mx>
2023-06-17 21:59:57 -0600

    Clase Pelicula
    Al final la clase Pelicula no fue usada debido a que todo se creo en la de videos

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,6 +1,20 @@
+/*
+Autor: MARIO ALBERTO PÉREZ BARRERA  A01799928
+PROGRAMACION ORIENTADA A OBJETOS
+SITUACION PROBLEMA
+
+
+SE IMPLEMENTA LA CLASE Pelicula
+*/
+
 #include "Pelicula.h"
 
-Pelicula::Pelicula(string id, string nombre, double duracion, string genero, double calificacion, string fechaEstreno) : 
-Video(id, nombre, duracion, genero, calificacion, fechaEstreno)
+Pelicula::Pelicula(string id, string nombre, int duracion,  vector<string> generos, double calificacion, string fechaEstreno) : Video(id, nombre, duracion, generos, calificacion, fechaEstreno)
 {
+}
+
+void Pelicula::verVideo()
+{
+   cout << "Pelicula: " << endl;
+   Video::verVideo();
 }
--- Pelicula.h
+++ Pelicula.h
@@ -1,3 +1,11 @@
+/*
+Autor: MARIO ALBERTO PÉREZ BARRERA  A01799928
+PROGRAMACION ORIENTADA A OBJETOS
+SITUACION PROBLEMA
+
+
+SE IMPLEMENTA LA CLASE Pelicula
+*/
 
 #include "Video.h"
 #ifndef PELICULA_H
@@ -6,7 +14,9 @@
 class Pelicula : public Video
 {
 public:
-    Pelicula(string id, string nombre, double duracion, string genero, double calificacion, string fechaEstreno);
+    Pelicula(string id, string nombre, int duracion,  vector<string> generos, double calificacion, string fechaEstreno);
+    void verVideo();
+    //friend ostream& operator<<(ostream& os, Pelicula& p);
 };
 
 #endif
