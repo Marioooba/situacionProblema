254b2e800c86872c8125183e8da5885464fc75b4
A01799928 <A01799928@tec.mx>
2023-06-17 22:02:50 -0600

    Clase Video.h  FINAL
    Se modificaron las variables anteriores y los géneros se guardaron en un vector para facilitar la búsqueda e impresión de estos. Se implementaron por completo las clases.
    Se usa el metodo vitual verVideo() que sera modificado por las demas clases

--- Video.cpp
+++ Video.cpp
@@ -1,51 +1,94 @@
+/*
+Autor: MARIO ALBERTO PÉREZ BARRERA  A01799928
+PROGRAMACION ORIENTADA A OBJETOS
+SITUACION PROBLEMA
+
+
+SE IMPLEMENTAN LOS CONSTRUCTORES DE LA CLASE VIDEO
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "Video.h"
 
-Video::Video(string id, string nombre, double duracion, string genero, double calificacion, string fechaEstreno)
+using namespace std;
+
+// Incluimos las bibliotecas necesarias y el archivo de encabezado "Video.h"
+
+Video::Video(string id, string nombre, int duracion, vector<string> generos, double calificacion, string fechaEstreno)
 {
-    this->id = id;
-    this->nombre = nombre;
-    this->duracion = duracion;
-    this->genero = genero;
-    this->calificacion = calificacion;
-    this->fechaEstreno = fechaEstreno;
+  // Constructor de la clase Video que recibe los parámetros y los asigna a las variables de instancia correspondientes.
+  this->id = id;
+  this->nombre = nombre;
+  this->duracion = duracion;
+  this->generos = generos;
+  this->calificacion = calificacion;
+  this->fechaEstreno = fechaEstreno;
 }
 
-string Video::getId()
-{
-    return id;
+void Video::setCalificacion(float calificacion) {
+  // Función para establecer la calificación del video.
+  this->calificacion = calificacion;
 }
 
-string Video::getNombre()
-{
-    return nombre;
+string Video::getIdVideo() {
+  // Función para obtener el ID del video.
+  return id;
 }
 
-double Video::getDuracion()
-{
-    return duracion;
+string Video::getNombre() {
+  // Función para obtener el nombre del video.
+  return nombre;
 }
 
-string Video::getGenero()
-{
-    return genero;
+float Video::getDuracion() {
+  // Función para obtener la duración del video.
+  return duracion;
 }
 
-double Video::getCalificacion()
-{
-    return calificacion;
+vector<string> Video::getGenerosVideo() {
+  // Función para obtener los géneros del video como un vector de cadenas.
+  return generos;
 }
 
-string Video::getFechaEstreno()
-{
-    return fechaEstreno;
+float Video::getCalificacion() {
+  // Función para obtener la calificación del video.
+  return calificacion;
 }
 
-void Video::verVideo()
-{
-    cout << "Nombre del video: " << nombre << "ID: " << id << endl;
+string Video::getFechaEstreno() {
+  // Función para obtener la fecha de estreno del video.
+  return fechaEstreno;
 }
 
-void Video::setCalificacion(double calif)
-{
-    this->calificacion = calificacion;
+void Video::videoGeneros() {
+  // Función para imprimir los géneros del video en la consola.
+  for (int i = 0; i < generos.size(); i++) {
+    cout << generos[i] << " ";
+  }
+  cout << endl;
+}
+
+
+bool Video::tieneGenero(string genero) {
+  // Función para verificar si el video tiene un género específico.
+  for (int i = 0; i < generos.size(); i++) {
+    if (generos[i] == genero) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void Video::verVideo() {
+  // Función virtual para ver el video.
+  cout << "Id: " << id << endl;
+  cout << "Nombre: " << nombre << endl;
+  cout << "Duracion: " << duracion << endl;
+  cout << "Generos: ";
+  videoGeneros();
+  cout << "Calificacion: " << calificacion << endl;
+  cout << "Fecha de estreno: " << fechaEstreno << endl;
 }
--- Video.h
+++ Video.h
@@ -1,35 +1,62 @@
+
+/*
+Autor: MARIO ALBERTO PÉREZ BARRERA  A01799928
+PROGRAMACION ORIENTADA A OBJETOS
+SITUACION PROBLEMA
+
+
+SE IMPLEMENTA LA CLASE VIDEO
+*/
 #ifndef VIDEO_H
 #define VIDEO_H
 
-#include <string>
-using std::string;
 #include <iostream>
+#include <string>
 #include <vector>
+
 using namespace std;
 
 class Video
 {
-protected:
-    string id;
-    string nombre;
-    double duracion;
-    string genero;
+    // Declaramos las variables de instancia para la clase Video
+    string id, nombre, fechaEstreno;
+    int duracion;
+    vector<string> generos;
     double calificacion;
-    string fechaEstreno;
 
 public:
-    // Constructor que recibe todos los parámetros
-    Video(string id, string nombre, double duracion, string genero, double calificacion, string fechaEstreno);
-
-    // Getters para acceder a nuestras variables
-    string getId();
+    // Constructor de la clase Video con parámetros
+    Video(string id, string nombre, int duracion, vector<string> generos, double calificacion, string fechaEstreno);
+    
+    // Función para establecer la calificación del video
+    void setCalificacion(float calificacion);
+    
+    // Función para obtener el ID del video
+    string getIdVideo();
+    
+    // Función para obtener el nombre del video
     string getNombre();
-    double getDuracion();
-    string getGenero();
-    double getCalificacion();
-    string getFechaEstreno();
+    
+    // Función para obtener la duración del video
+    float getDuracion();
+    
+    // Función para obtener los géneros del video como un vector de cadenas
+    vector<string> getGenerosVideo();
+    
+    // Función para imprimir los géneros del video en la consola
+    void videoGeneros();
+    
+    // Función virtual para ver el video (será implementada en clases derivadas)
     virtual void verVideo();
-    void setCalificacion(double calificacion);
+    
+    // Función para obtener la calificación del video
+    float getCalificacion();
+    
+    // Función para obtener la fecha de estreno del video
+    string getFechaEstreno();
+    
+    // Función para verificar si el video tiene un género específico
+    bool tieneGenero(string genero);
 };
 
-#endif
+#endif // !VIDEO_H
